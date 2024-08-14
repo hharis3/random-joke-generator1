d39d5f071bd02c30bb76e3ce187d04e21d12c134
hharis3 <huzaifaharis01@gmail.com>
2024-08-13 22:53:24 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import requests
+
+def get_joke():
+    response = requests.get("https://official-joke-api.appspot.com/jokes/random")
+    joke = response.json()
+    return f"{joke['setup']} - {joke['punchline']}"
+
+if __name__ == "__main__":
+    print(get_joke())
